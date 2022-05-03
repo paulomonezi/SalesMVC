07a000e213f251c641f1b3fef21f59543b6a7c53
Paulo Monezi <paulo.monezi@icloud.com>
2022-05-02 20:54:25 -0400

    added seeding service

--- SalesMVC/Data/SalesMVCContext.cs
+++ SalesMVC/Data/SalesMVCContext.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SalesMVC.Models;
+﻿using Microsoft.EntityFrameworkCore;
+
 
 namespace SalesMVC.Models
 {
--- SalesMVC/Startup.cs
+++ SalesMVC/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SalesMVC.Models;
+using SalesMVC.Data;
 
 namespace SalesMVC
 {
@@ -26,14 +27,17 @@ namespace SalesMVC
             string connectionString = Configuration.GetConnectionString("SalesWebMVCContext");
 
             services.AddDbContext<SalesMVCContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
