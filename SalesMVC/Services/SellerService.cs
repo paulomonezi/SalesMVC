cc89b3423985496d331a4ad0c997c7c2c11c2239
Paulo Monezi <paulo.monezi@icloud.com>
2022-05-02 22:12:53 -0400

    Seller Service and basic FindAll

---
+++ SalesMVC/Services/SellerService.cs
@@ -0,0 +1,21 @@
+﻿using SalesMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesMVCContext _context;
+
+        public SellerService(SalesMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- SalesMVC/Startup.cs
+++ SalesMVC/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SalesMVC.Models;
 using SalesMVC.Data;
+using SalesMVC.Services;
 
 namespace SalesMVC
 {
@@ -29,6 +30,7 @@ namespace SalesMVC
             services.AddDbContext<SalesMVCContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
