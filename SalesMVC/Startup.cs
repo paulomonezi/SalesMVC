d0394d8ed588aa96badeb9359f89538c61b03274
Paulo Monezi <paulo.monezi@icloud.com>
2022-05-04 09:48:00 -0400

    App locale, nbumber and date formating

--- SalesMVC/Startup.cs
+++ SalesMVC/Startup.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Globalization;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
 using SalesMVC.Models;
 using SalesMVC.Data;
 using SalesMVC.Services;
+
 
 namespace SalesMVC
 {
@@ -37,6 +41,20 @@ namespace SalesMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
+
+            //Localization
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
+
+                
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
