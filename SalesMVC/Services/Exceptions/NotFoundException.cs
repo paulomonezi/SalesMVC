32dd9e26730924812b4e2c280b67dec0bb232165
Paulo Monezi <paulo.monezi@icloud.com>
2022-05-03 23:42:21 -0400

    Update seller and custom service exception

---
+++ SalesMVC/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesMVC.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
