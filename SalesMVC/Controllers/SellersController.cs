d59f2c84e3166f585b9f919f31c4133eb21a0a2f
Paulo Monezi <paulo.monezi@icloud.com>
2022-05-04 22:33:16 -0400

    Exception handling for delete (referencial integrity)

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -67,9 +67,16 @@ namespace SalesMVC.Controllers
 
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
-        }
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new {message = e.Message});
+            }
+            }
 
         public async Task<IActionResult> Details(int? id)
         {
